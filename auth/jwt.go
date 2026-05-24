3561628324d24d223da04d6ac7d3ce2e30fb96b1
awoladhossain <awoladh04@gmail.com>
2026-05-25 02:01:38 +0600

    verify token initial

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,10 +1,19 @@
 package auth
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"github.com/awoladhossain/turfbook-streaming/models"
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type Claims struct {
 	Sub   string `json:"sub"`
 	Email string `json:"email"`
 	Role  string `json:"role"`
 	jwt.RegisteredClaims
+}
+
+// token verification - nestjs same secret and algorithm
+
+func VerifyToken(tokenString string) (*models.AuthUser, error) {
+	token, err := jwt.ParseWithClaims()
 }
