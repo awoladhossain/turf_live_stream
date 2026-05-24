d6dcee4d268390a6000c17341a81968e38c47352
awoladhossain <awoladh04@gmail.com>
2026-05-25 01:59:41 +0600

    feat: auth- jwt claims struct added

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,1 +1,10 @@
 package auth
+
+import "github.com/golang-jwt/jwt/v5"
+
+type Claims struct {
+	Sub   string `json:"sub"`
+	Email string `json:"email"`
+	Role  string `json:"role"`
+	jwt.RegisteredClaims
+}
